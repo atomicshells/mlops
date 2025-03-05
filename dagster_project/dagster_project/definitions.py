09152af8bde29594f1b26f3fe3a101ef261bd927
atomicshells <shemharoldcastillo@gmail.com>
2025-03-05 17:23:09 +0800

    Updated definitions.py.

--- dagster_project/dagster_project/definitions.py
+++ dagster_project/dagster_project/definitions.py
@@ -1,9 +1,12 @@
-from dagster import Definitions, load_assets_from_modules
-
-from dagster_project import assets  # noqa: TID252
-
-all_assets = load_assets_from_modules([assets])
+from dagster import Definitions
+from dagster_project.assets.load_data_asset import load_data
+from dagster_project.assets.preprocess_asset import preprocess_data
+from dagster_project.assets.features_asset import build_features
+from dagster_project.assets.train_model_asset import train_model_asset
+from dagster_project.assets.predict_asset import predict_and_evaluate
+from dagster_project.jobs.ml_pipeline_job import ml_pipeline_job
 
 defs = Definitions(
-    assets=all_assets,
+    assets=[load_data, preprocess_data, build_features, train_model_asset, predict_and_evaluate],
+    jobs=[ml_pipeline_job]
 )
