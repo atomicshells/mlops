290f66cc6cb54c741b7be0f7122fe6689b3f6269
atomicshells <shemharoldcastillo@gmail.com>
2025-03-05 16:25:25 +0800

    Updated train_models.py.

--- src/models/train_models.py
+++ src/models/train_models.py
@@ -1,9 +1,9 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 def train_model(X, y, test_size=0.2, random_state=42):
     """
-    Splits data into train and test sets, trains a RandomForestClassifier, and returns the trained model and test set.
+    Splits data into train and test sets, trains a RandomForestRegressor, and returns the trained model and test set.
 
     Args:
         X (pd.DataFrame): Feature matrix.
@@ -12,13 +12,13 @@ def train_model(X, y, test_size=0.2, random_state=42):
         random_state (int): Random seed for reproducibility.
 
     Returns:
-        RandomForestClassifier: Trained model.
+        RandomForestRegressor: Trained model.
         pd.DataFrame: X_test (features for test set).
         pd.Series: y_test (labels for test set).
     """
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-    model = RandomForestClassifier(n_estimators=50, random_state=random_state)
+    model = RandomForestRegressor(n_estimators=50, random_state=random_state)
     model.fit(X_train, y_train)
 
     return model, X_test, y_test
